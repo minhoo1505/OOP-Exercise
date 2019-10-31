ad6afd317e0df6c6233e0a59eb39afb7da948d9f
Minhhoo <yttyj>
2019-10-31 13:35:42 +0700

    OOP Exercise

---
+++ TheEmployee/TheEmployee/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheEmployee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e = new Employee();
+            Console.WriteLine(e.ToString());
+            Console.ReadLine();
+        }
+    }
+}
